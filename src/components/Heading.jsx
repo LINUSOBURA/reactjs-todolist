da225a7bf8451fc6b7d90c6d955214c55eec443f
LINUSOBURA <linusobura@gmail.com>
2024-06-15 14:50:58 +0300

    Initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Heading() {
+  return (
+    <header>
+      <div className="heading">
+        <h1>My Todo App</h1>
+      </div>
+    </header>
+  );
+}
